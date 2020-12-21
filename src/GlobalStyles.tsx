de1ccc37799c732fdba19082a84018e9812b857c
Liam T <liam.t10@icloud.com>
2020-12-21 22:47:19 +0000

    added FlexHeightElement component and global style rules to allow easy 100% height elements

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,8 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  html, body, #root {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 auto;
+  }
+
+  html {
+    min-height: 100%;
+    font-size: 10px;
+  }
+
   body {
     font-family: sans-serif;
+    font-size: 1.6rem;
   }
 `;
 
---
+++ src/components/FlexHeightElement.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+interface Props {
+  className?: string,
+  onClick?: () => void,
+}
+
+
+const FlexHeightElement:React.FC<Props> = ({ children, className, onClick }) => (
+  <FlexHeightElementOuter className={className} onClick={onClick}>
+    {children}
+  </FlexHeightElementOuter>
+);
+export default FlexHeightElement;
+
+
+const FlexHeightElementOuter = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+`;
