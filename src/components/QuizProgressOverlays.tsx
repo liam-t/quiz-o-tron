dede2c038ec63990827d1a9325baf8521244a44e
Liam T <liam.t10@icloud.com>
2020-12-23 23:04:50 +0000

    finalised logic for counting round/question numbers in QuizProgressOverlays;
    some styling in QuizProgressOverlays;

--- src/components/QuizProgressOverlays.tsx
+++ src/components/QuizProgressOverlays.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import { Slide as SlideModel } from 'models';
-
+import { SlideTypes } from 'types';
 
 interface Props {
   activeSlideIndex: number,
@@ -10,10 +10,27 @@ interface Props {
 
 
 const QuizProgressOverlays:React.FC<Props> = ({ activeSlideIndex, slides }) => {
-  const currentSlide = null;
+  type TallyObj = { round: number, question: number };
+  const reducer = (acc: TallyObj, item: SlideModel): TallyObj => {
+    if (item.slideType === 'round') {
+      acc.round += 1;
+      acc.question = 0;
+    } else if (item.slideType === 'question') {
+      acc.question += 1;
+    }
+    return acc;
+  };
+  const tally = slides.slice(0, activeSlideIndex + 1).reduce(reducer, {
+    round: 0,
+    question: 0,
+  });
+  const isLastSlide = activeSlideIndex === slides.length - 1;
+  const showQuestionNumber = !!(tally.question) && !isLastSlide;
+  const showRoundNumber = !!(tally.round) && !isLastSlide;
   return (
     <QuizProgressOverlaysOuter>
-      <QuestionNumber>Question {activeSlideIndex + 1}</QuestionNumber>
+      {showQuestionNumber && <QuestionNumber>Question {tally.question}</QuestionNumber>}
+      {showRoundNumber && <RoundNumber>Round {tally.round}</RoundNumber>}
     </QuizProgressOverlaysOuter>
   );
 };
@@ -23,8 +40,18 @@ export default QuizProgressOverlays;
 const QuizProgressOverlaysOuter = styled.div`
   color: white;
 `;
+const margin = 10;
 const QuestionNumber = styled.div`
   position: absolute;
-  top: 20px;
-  left: 20px;
+  top: ${margin}px;
+  left: ${margin}px;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+const RoundNumber = styled(QuestionNumber)`
+  top: auto;
+  left: auto;
+  bottom: ${margin}px;
+  right: ${margin}px;
 `;
