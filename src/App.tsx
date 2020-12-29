5ed7be98132f6a0d3cde351ffd5acb9662f6096d
Liam T <liam.t10@icloud.com>
2020-12-29 22:11:30 +0000

    implemented QuizInput component with data switcher and router in App.tsx; data now is auto selected when /input route is hit and replaces previous Quiz data; todo: quiz input UI(?) etc

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,43 @@ import styled from 'styled-components/macro';
 import Container from 'components/Container';
 import Quiz from 'components/Quiz';
 import FlexHeightElement from 'components/FlexHeightElement';
+import QuizInput from 'components/QuizInput';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { Quiz as QuizModel } from 'models';
 import data from './assets/data/dummyData.json';
 import GlobalStyles from './GlobalStyles';
 import 'normalize.css';
 
-// class myData implements QuizType {
 
-// }
-
-const App:React.FC = () => (
-  <AppWrapper>
-    <GlobalStyles />
-    <Container flexHeight>
-      <Quiz data={data} />
-    </Container>
-  </AppWrapper>
-);
+const App:React.FC = () => {
+  const [quizData, setQuizData] = React.useState<QuizModel>(data);
+  const handleQuizInputChange = (dataReturn: QuizModel) => setQuizData(dataReturn);
+  const memoizedHandleQuizInputChange = React.useCallback(handleQuizInputChange, []);
+  return (
+    <Router>
+      <AppWrapper>
+        <GlobalStyles />
+        <Container flexHeight>
+          <Link to="/">home</Link>
+          <Link to="/input">input</Link>
+          <Switch>
+            <Route exact path="/input">
+              <QuizInput onChange={memoizedHandleQuizInputChange} />
+            </Route>
+            <Route>
+              <Quiz data={quizData} />
+            </Route>
+          </Switch>
+        </Container>
+      </AppWrapper>
+    </Router>
+  );
+};
 
 export default App;
 
